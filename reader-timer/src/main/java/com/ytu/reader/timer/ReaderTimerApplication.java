3d59e443616485bd827d0a3da4aa08915e9100a2
mnjic-503 <269810721@qq.com>
2020-05-16 19:11:25 +0800

    项目初始结构创建

---
+++ reader-timer/src/main/java/com/ytu/reader/timer/ReaderTimerApplication.java
@@ -0,0 +1,13 @@
+package com.ytu.reader.timer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderTimerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReaderTimerApplication.class, args);
+    }
+
+}
---
+++ reader-timer/src/test/java/com/ytu/reader/timer/ReaderTimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytu.reader.timer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderTimerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
